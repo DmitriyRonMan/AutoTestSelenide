7ff275b46ca657d331e80b5baba994554df7d366
Dmitriy RonMan <dmitriykolesnikov7@gmail.com>
2023-09-22 22:49:23 +0500

    readme

---
+++ src/test/java/AutoTestSelenide.java
@@ -0,0 +1,21 @@
+import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+
+
+public class AutoTestSelenide {
+    @BeforeAll
+    static void setUp() {
+        open("https://the-internet.herokuapp.com/add_remove_elements/");
+    }
+    @Test
+    void shouldTest() {
+        Configuration.holdBrowserOpen = true;
+        $(".button").click();
+    }
+
+
+}
